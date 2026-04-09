8b989488312671f7ff4bad0b1971f1a3f33538bd
Jannatul Ferdous Nabila <samanthanabila47@gmail.com>
2026-04-09 17:03:23 +0600

    if else/ Switch

---
+++ Section-4/01-if-else/script.js
@@ -0,0 +1,10 @@
+let hour = 1;
+if (hour >= 6 && hour < 12) {
+  console.log("Good Morning");
+} else if (hour >= 12 && hour < 18) {
+  console.log("Good Afternoon");
+} else if (hour >= 18 && hour < 24) {
+  console.log("Good ebening");
+} else {
+  console.log("Good Night");
+}
---
+++ Section-4/02-switch/script.js
@@ -0,0 +1,14 @@
+let role = "nabila";
+switch (role) {
+  case "guest":
+    console.log("Guest User");
+    break;
+  case "moderator":
+    console.log("moderator User");
+    break;
+  case "admin":
+    console.log("admin User");
+    break;
+  default:
+    console.log("unknown");
+}
